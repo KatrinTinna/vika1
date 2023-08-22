53de8547ca4c152aaa82199256459472b178befe
KatrinTinna <katrintinnas@gmail.com>
2023-08-22 11:18:28 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def leggja_saman():
+    tala1 = int(input("Sláðu inn heiltölu: "))
+    tala2 = int(input("Sláðu inn heiltölu: "))
+    tala3 = int(input("Sláðu inn heiltölu: "))
+    samlagning = tala1 + tala2 + tala3
+    print ("Halló stærðfræði %d" %(samlagning))
+
+leggja_saman()
